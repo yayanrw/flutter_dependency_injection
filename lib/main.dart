780c68070c54763629e179d3c18d0f9b1153c480
yayanrw <yayanraw@gmail.com>
2022-04-13 11:22:09 +0700

    provider & configure injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:dependency_injection/configs/env.dart';
+import 'package:dependency_injection/injection/injection.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'pages/home_page.dart';
 import 'providers/data_provider.dart';
 
 void main() {
+  configureInjection(Env.prod);
   runApp(const MyApp());
 }
 
@@ -13,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Provider<DataProvider>(
-      create: (context) => DataProvider(),
+      create: (context) => getIt<DataProvider>(),
       child: const MaterialApp(
         title: "Belajar Dependency Injection",
         home: HomePage(),
